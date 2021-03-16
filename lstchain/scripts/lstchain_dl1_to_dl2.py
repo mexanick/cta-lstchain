81b175c98912f44a70808e1b34e984caf07f3f44
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2021-03-16 09:25:28 +0000

    Fix telescope name/dataset keys for DL1 to DL2 conversion

--- lstchain/scripts/lstchain_dl1_to_dl2.py
+++ lstchain/scripts/lstchain_dl1_to_dl2.py
@@ -31,7 +31,6 @@ from lstchain.io import (
     get_dataset_keys,
 )
 from lstchain.io.io import (
-    dl1_params_lstcam_key,
     dl1_params_src_dep_lstcam_key,
     dl1_images_lstcam_key,
     dl2_params_lstcam_key,
@@ -77,112 +76,105 @@ def main():
 
     config = replace_config(standard_config, custom_config)
 
-    data = pd.read_hdf(args.input_file, key=dl1_params_lstcam_key)
-
-    # if real data, add deltat t to dataframe keys
-    data = add_delta_t_key(data)
-
-    # Dealing with pointing missing values. This happened when `ucts_time` was invalid.
-    if 'alt_tel' in data.columns and 'az_tel' in data.columns \
-            and (np.isnan(data.alt_tel).any() or np.isnan(data.az_tel).any()):
-        # make sure there is a least one good pointing value to interp from.
-        if np.isfinite(data.alt_tel).any() and np.isfinite(data.az_tel).any():
-            data = impute_pointing(data)
-        else:
-            data.alt_tel = - np.pi / 2.
-            data.az_tel = - np.pi / 2.
-
-    # Load the trained RF for reconstruction:
-    fileE = args.path_models + "/reg_energy.sav"
-    fileD = args.path_models + "/reg_disp_vector.sav"
-    fileH = args.path_models + "/cls_gh.sav"
-
-    reg_energy = joblib.load(fileE)
-    reg_disp_vector = joblib.load(fileD)
-    cls_gh = joblib.load(fileH)
-
-    subarray_info = SubarrayDescription.from_hdf(args.input_file)
-    tel_id = config["allowed_tels"][0] if "allowed_tels" in config else 1
-    focal_length = subarray_info.tel[tel_id].optics.equivalent_focal_length
-
-    # Apply the models to the data
-
-    # Source-independent analysis
+    dataset_keys = get_dataset_keys(args.input_file)
     if not config['source_dependent']:
-        data = filter_events(data,
-                             filters=config["events_filters"],
-                             finite_params=config['regression_features'] + config['classification_features'],
-                             )
-
-        dl2 = dl1_to_dl2.apply_models(data, cls_gh, reg_energy, reg_disp_vector, focal_length=focal_length,
-                                      custom_config=config)
-
-    # Source-dependent analysis
-    if config['source_dependent']:
-        data_srcdep = pd.read_hdf(args.input_file, key=dl1_params_src_dep_lstcam_key)
-        data_srcdep.columns = pd.MultiIndex.from_tuples(
-            [tuple(col[1:-1].replace('\'', '').replace(' ', '').split(",")) for col in data_srcdep.columns])
-
-        dl2_srcdep_dict = {}
-
-        for i, k in enumerate(data_srcdep.columns.levels[0]):
-            data_with_srcdep_param = pd.concat([data, data_srcdep[k]], axis=1)
-            data_with_srcdep_param = filter_events(data_with_srcdep_param,
-                                                   filters=config["events_filters"],
-                                                   finite_params=config['regression_features'] + config[
-                                                       'classification_features'],
-                                                   )
-            dl2_df = dl1_to_dl2.apply_models(data_with_srcdep_param, cls_gh, reg_energy, reg_disp_vector,
-                                             focal_length=focal_length, custom_config=config)
-
-            dl2_srcdep = dl2_df.drop(data.keys(), axis=1)
-            dl2_srcdep_dict[k] = dl2_srcdep
-
-            if i == 0:
-                dl2_srcindep = dl2_df.drop(data_srcdep[k].keys(), axis=1)
-
-    os.makedirs(args.output_dir, exist_ok=True)
-    output_file = os.path.join(args.output_dir, os.path.basename(args.input_file).replace('dl1', 'dl2'))
-
-    if os.path.exists(output_file):
-        raise IOError(output_file + ' exists, exiting.')
-
-    dl1_keys = get_dataset_keys(args.input_file)
-
-    if dl1_images_lstcam_key in dl1_keys:
-        dl1_keys.remove(dl1_images_lstcam_key)
-
-    if dl1_params_lstcam_key in dl1_keys:
-        dl1_keys.remove(dl1_params_lstcam_key)
-
-    if dl1_params_src_dep_lstcam_key in dl1_keys:
-        dl1_keys.remove(dl1_params_src_dep_lstcam_key)
-
-    with open_file(args.input_file, 'r') as h5in:
-        with open_file(output_file, 'a') as h5out:
-
-            # Write the selected DL1 info
-            for k in dl1_keys:
-                if not k.startswith('/'):
-                    k = '/' + k
-
-                path, name = k.rsplit('/', 1)
-                if path not in h5out:
-                    grouppath, groupname = path.rsplit('/', 1)
-                    g = h5out.create_group(
-                        grouppath, groupname, createparents=True
-                    )
-                else:
-                    g = h5out.get_node(path)
-
-                h5in.copy_node(k, g, overwrite=True)
-
-    if not config['source_dependent']:
-        write_dl2_dataframe(dl2, output_file)
-
+        params_keys = [k for k in dataset_keys if 'parameters/' in k]
     else:
-        write_dl2_dataframe(dl2_srcindep, output_file)
-        write_dataframe(pd.concat(dl2_srcdep_dict, axis=1), output_file, dl2_params_src_dep_lstcam_key)
+        params_keys = [k for k in dataset_keys if 'parameters_src_dependent/' in k]
+    # Drop unwanted DL1 keys
+    dataset_keys = [k for k in dataset_keys if 'image/' not in k]
+    dataset_keys = [k for k in dataset_keys if 'parameters/' not in k]
+    dataset_keys = [k for k in dataset_keys if 'parameters_src_dependent/' not in k]
+
+    for tel_id in config['source_config']['EventSource']['allowed_tels']:
+        for k in params_keys:
+            if k.endswith(f'{tel_id}'):
+
+                data = pd.read_hdf(args.input_file, key=k)
+
+                # if real data, add deltat t to dataframe keys
+                data = add_delta_t_key(data)
+
+                # Dealing with pointing missing values. This happened when `ucts_time` was invalid.
+                if 'alt_tel' in data.columns and 'az_tel' in data.columns \
+                        and (np.isnan(data.alt_tel).any() or np.isnan(data.az_tel).any()):
+                    # make sure there is a least one good pointing value to interp from.
+                    if np.isfinite(data.alt_tel).any() and np.isfinite(data.az_tel).any():
+                        data = impute_pointing(data)
+                    else:
+                        data.alt_tel = - np.pi/2.
+                        data.az_tel = - np.pi/2.
+
+                #Load the trained RF for reconstruction:
+                fileE = f"{args.path_models}/{k.split('/')[-1]}/reg_energy.sav"
+                fileD = f"{args.path_models}/{k.split('/')[-1]}/reg_disp_vector.sav"
+                fileH = f"{args.path_models}/{k.split('/')[-1]}/cls_gh.sav"
+
+                reg_energy = joblib.load(fileE)
+                reg_disp_vector = joblib.load(fileD)
+                cls_gh = joblib.load(fileH)
+
+                #Apply the models to the data
+
+                #Source-independent analysis
+                if not config['source_dependent']:
+                    data = filter_events(data,
+                                         filters=config["events_filters"],
+                                         finite_params=config['regression_features'] + config['classification_features'],
+                                     )
+
+                    dl2 = dl1_to_dl2.apply_models(data, cls_gh, reg_energy, reg_disp_vector, custom_config=config)
+
+                #Source-dependent analysis
+                if config['source_dependent']:
+                    data_srcdep = pd.read_hdf(args.input_file, key=k)
+                    data_srcdep.columns = pd.MultiIndex.from_tuples([tuple(col[1:-1].replace('\'', '').replace(' ','').split(",")) for col in data_srcdep.columns])
+
+                    dl2_srcdep_dict = {}
+
+                    for i, j in enumerate(data_srcdep.columns.levels[0]):
+                        data_with_srcdep_param = pd.concat([data, data_srcdep[j]], axis=1)
+                        data_with_srcdep_param = filter_events(data_with_srcdep_param,
+                                                           filters=config["events_filters"],
+                                                           finite_params=config['regression_features'] + config['classification_features'],
+                                                       )
+                        dl2_df = dl1_to_dl2.apply_models(data_with_srcdep_param, cls_gh, reg_energy, reg_disp_vector, custom_config=config)
+
+                        dl2_srcdep = dl2_df.drop(data.keys(), axis=1)
+                        dl2_srcdep_dict[j] = dl2_srcdep
+
+                        if i==0:
+                            dl2_srcindep = dl2_df.drop(data_srcdep[j].keys(), axis=1)
+
+                os.makedirs(args.output_dir, exist_ok=True)
+                output_file = os.path.join(args.output_dir, os.path.basename(args.input_file).replace('dl1','dl2'))
+
+                with open_file(args.input_file, 'r') as h5in:
+                    with open_file(output_file, 'a') as h5out:
+
+                        # Write the selected DL1 info
+                        for key_to_write in dataset_keys:
+                            if not key_to_write.startswith('/'):
+                                key_to_write = '/' + key_to_write
+
+                            path, name = key_to_write.rsplit('/', 1)
+                            if path not in h5out:
+                                grouppath, groupname = path.rsplit('/', 1)
+                                g = h5out.create_group(
+                                    grouppath, groupname, createparents=True
+                                    )
+                            else:
+                                g = h5out.get_node(path)
+
+                            h5in.copy_node(key_to_write, g, overwrite=True)
+
+                key_to_write = k.replace('dl1', 'dl2')
+                if not config['source_dependent']:
+                    write_dataframe(dl2, output_file, key_to_write)
+                else:
+                    write_dataframe(dl2_srcindep, output_file,
+                                        key_to_write.replace('parameters_src_dependent', 'parameters'))
+                    write_dataframe(pd.concat(dl2_srcdep_dict, axis=1), output_file, k)
 
 
 if __name__ == '__main__':
